3eef2819260012c79abd8c0331a41809e479cb7f
GustavooHPA <100046745+GustavooHPA@users.noreply.github.com>
2022-07-14 11:11:27 -0300

    Add files via upload

---
+++ descriptografar.js
@@ -0,0 +1,11 @@
+function Enviar() {
+    var textcriptp1 = window.document.getElementById("TextCript")
+    var textcript = String(textcriptp1.value)
+    var textdecriparea = window.document.getElementById("TextDecrip")
+    var message = "MSKZEPO nquhy ME Y YVBO nbce K tm hs eqy  MKOHG chy kw yn e YDEKO eqne  ych yc eq nwy XXC! O lbkyqrh yemqn lycygyh yry fehgkwfs hfkwbkmsr YPQ wyn ngks gyjc clncje tpisn nsh lkwbto qxhg tmyy  s nktmc nblrahoh lnxhgie 4324523 5255  0654064 34423 94vdoh 4942834238 dnj6 5 13132131 CXXC!"
+    if (textcript == message) {
+      textdecriparea.innerHTML = "COMPARE jskda CA A ORDE jdsa M ji ji aso  IMEDI sda as ad a ATAME asda  asd as as dsa NTE! É hdaushd auisd hasuiod ahu hudiashi dhasdaiuh URG sad jiao iofe shpsfg jlkij pid nasdjk sndi jiao  o papos jdbncxkj bjzxcku 4324523 5255  0654064 34423 94rfed 4942834238 fdf6 5 13132131 ENTE! FOMOS HA fijsadfjias djdaso a daso dsdp adajds mcxoizj ci"
+    } else {
+      textdecriparea.innerText = "Isso não foi enviado pela Ordem!"
+    }
+  }
